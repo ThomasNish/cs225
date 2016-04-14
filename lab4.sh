705597db3c5f57b3d945646b6e86a474f3de5ab3
Thomas Nishimura <t.nishimura1886@edmail.edcc.edu>
2016-04-14 15:45:21 -0700

    Finished lab 4

---
+++ lab4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter a number between 50 and 100: "
+read NUMBER
+
+NUMBER=$(echo $NUMBER | sed 's/^0*//')
+NUMBER=$(echo $NUMBER | sed 's/[.].*$//')
+NUMBER=$(echo $NUMBER | sed 's/[[:punct:][:alpha:]]//')
+
+if [ $NUMBER -eq $NUMBER 2>/dev/null ] && [ $NUMBER -le 100 ] && [ $NUMBER -ge 50 ] ;then
+	echo "You entered a valid number"
+else 
+	echo "Your number was invalid"
+fi
+
