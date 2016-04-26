8c932f808e8cdf166b515d5c8a91b4f8174f2439
Thomas Nishimura <t.nishimura1886@edmail.edcc.edu>
2016-04-26 16:15:55 -0700

    Fixed lab 6, now outputs correct numbers

--- lab6.sh
+++ lab6.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-MPVAR=$(ls /root/Projects/CS225/medialab | grep \.mp)
+MPVAR=$(ls -1 /root/Projects/CS225/medialab)
 LAB=/root/Projects/CS225/medialab
 XML=/root/Projects/CS225/medialab/media.xml
-
-STRMEDIA=$( grep "filename" /root/Projects/CS225/medialab/media.xml | awk -F'>' '{ print $2 }' | awk -F'<' '{ print $1 }' | sort | uniq | grep -vwE "(NULL)" )
+XMLcounter=0
+MEDIAcounter=0
+STRMEDIA=$( grep "filename" /root/Projects/CS225/medialab/media.xml | grep "mp[g3]" | awk -F'>' '{ print $2 }' | awk -F'<' '{ print $1 }'  | sort | uniq | grep -vwE "(NULL)" )
 echo "Files not in medialab:"
-for XMLFILES in $STRMEDIA ;do
-        if [[ -e $LAB/"$XMLFILES" ]] ;then
-                :
+for XMLFILE in $STRMEDIA ;do
+        if [[ -e $LAB/"$XMLFILE" ]] ;then
+                (( ishere++ ))
         else
                 echo $XMLFILE
 		(( XMLcounter++ ))
@@ -16,12 +17,12 @@ for XMLFILES in $STRMEDIA ;do
 done
 echo "Files not in media.xml:"
 for MEDIAFILES in $MPVAR ;do
-	if [[ -e /root/Projects/CS225/medialab/media.xml/"$MEDIAFILES" ]] ;then
-		:
+	if grep -q $MEDIAFILES $XML  ;then
+		((ishere1++))
 	else
 		echo $MEDIAFILES
 		(( MEDIAcounter++ ))
 	fi
-done
+done  
 echo "$XMLcounter media files in media.xml that are NOT in medialab directory"
-echo "$MEDIAcounter media files in medialab that are NOT in medialab directory"
+echo "$MEDIAcounter media files in medialab that are NOT in media.xml"
