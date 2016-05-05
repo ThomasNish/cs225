2cd5b25cec0fe379614c2d7cdb188cf875802d84
Thomas Nishimura <t.nishimura1886@edmail.edcc.edu>
2016-05-05 15:42:21 -0700

    Got lab9.sh to handle arguments

--- lab9.sh
+++ lab9.sh
@@ -5,15 +5,26 @@ while getopts :dvhn:l: opt ;do
                 d) set -x ;;
                 v) VERBOSE=true
 		echo "VERBOSE is set true" ;;
-                h) echo "Your options are -d, -v, -h, -n, and -l" ;;
+                h) echo "Your options are:
+		 -d Setting debugging
+		 -v Setting Verbose to true
+		 -h Help 
+		 -n Entering a name 
+		 -l DIR (Directory Required)" ;;
                 n) ARG=$OPTARG 
 		echo "$ARG is the name entered" ;;
-                l) DIR=$OPTARG
-		echo "Would you like to view $DIR? (Yes or No)" ;;
-		read ANS
-		if (( ANS = [YyEeSs] ))
-			cat $DIR
-		\?) echo "That is not a valid option"
+                l) 	DIR=$OPTARG
+			if [[ -z $DIR ]] ;then
+				echo "You didn't enter a path"
+			fi
+			echo "Would you like to view $DIR? (Yes or No)" 
+			read ANS 
+			if echo $ANS | grep -iq "^y" ;then
+				ls $DIR/*
+			else 
+				exit 1 
+			fi ;;
+		\?) echo "That is not a valid option!" ;;
         esac
 done
 shift $(($OPTIND-1))
