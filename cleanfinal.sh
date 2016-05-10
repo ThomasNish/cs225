11ec913810edfd0289aafbf4e2023c6ccf8157b4
Thomas Nishimura <t.nishimura1886@edmail.edcc.edu>
2016-05-10 16:16:11 -0700

    Cleaning everything in the final back to original state

---
+++ cleanfinal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+$(userdel Student)
+rm -Rf /home/Student
+$(userdel $NAME)
+rm -Rf /home/$NAME
+$(umount /home)
+$(umount /var/reports)
+(echo d; echo 1; echo w; echo y) | gdisk /dev/xvdb
+(echo d; echo 2; echo w; echo y) | gdisk /dev/xvdb
+rm -Rf /media/temp
+rm -Rf /media/var
+rm -Rf /root/Projects/CS225/messages.log
+rm -Rf /var/reports
+rm -f /root/Projects/CS225/finalcli.txt
+$(groupdel netadmin)
+$(yum groupremove Xfce); echo y;
+exit 0
