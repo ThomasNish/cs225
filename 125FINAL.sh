fda66eed81234f25aef89975da90037e6d9e50d8
Thomas Nishimura <t.nishimura1886@edmail.edcc.edu>
2016-05-12 17:23:07 -0700

    Fixing errors

--- 125FINAL.sh
+++ 125FINAL.sh
@@ -8,6 +8,7 @@ else
 	MESSAGE=$(grep "DROPPED" messages.log | grep "Dec" | wc -l)
 	echo "There are $MESSAGE dropped packets in Dec" > /root/Projects/CS225/finalcli.txt
 fi
+
 # Objective 2
 (echo n; echo p; echo 1; echo 2048; echo +100MB; echo ; echo w) | fdisk /dev/xvdb
 (echo n; echo p; echo 2; echo ; echo +100MB; echo ; echo w) | fdisk /dev/xvdb
@@ -15,24 +16,26 @@ fi
 # Objective 3
 echo "Enter your name: "
 read NAME
-if [[ $(grep -qv $NAME /etc/passwd) -eq 0 ]] ;then 
+if grep -qv $NAME /etc/passwd ;then 
 	:
 else
 	$(useradd $NAME)
 fi
+
 # Objective 4
-#if [[ $(grep -qv Student /etc/passwd) -eq 0 ]] ;then
- #       :
-#else
+if grep -qv Student /etc/passwd ;then
+       :
+else
         $(useradd Student)
-#fi
-if [[ $(grep -qv netadmin /etc/group -eq 0) ]] ;then
+fi
+if grep -qv netadmin /etc/group ;then
         :
 else
         $(groupadd netadmin)
 fi
-usermod netadmin Student 
+usermod -a  netadmin Student 
 $(yum groupinstall Xfce); echo y;
+
 # Objective 5
 $(mkdir /media/temp)
 mkfs.ext4 /dev/xvdb1
@@ -41,17 +44,20 @@ $(cp -Rf /home/* /media/temp)
 $(umount /media/temp)
 $(mount /dev/xvdb1 /home)
 echo "/dev/xvdb1 /home /ext4 defaults 0 0" >> /etc/fstab
+
 # Objective 6
 if [[ -f /media/var ]] ;then
         :
 else
         $(mkdir /media/var)
 fi
+
 if [[  -f /var/reports  ]] ;then
         :
 else
         $(mkdir /var/reports)
 fi
+
 if [[ -f /var/reports/Marketing ]] ;then
         :
 else
@@ -67,7 +73,6 @@ $(mount /dev/xvdb2 /var/reports)
 
 # Objective 7
 $(chown Student. /home/Student)
-$(read -s -p "Enter password: " mypassword)
-echo $mypassword
+$(passwd Student) 
 #$(su - Student)
 #$(cd /Desktop) 
